3fe7d66c88d0692977e8ac74ed2e8d651e3ef713
elderyu <meridum93@gmail.com>
2025-05-02 08:16:00 +0200

    dev, equipping weapon

--- scenes/inventory_slot.gd
+++ scenes/inventory_slot.gd
@@ -40,6 +40,8 @@ func slot_item_put(new_item):
 	item_description_set(inv_item)
 	if equipment_slot_type != null:
 		print("todo equip item - adjust statistics")
+	if equipment_slot_type == EquipmentType.Type.WEAPON:
+		print("show on back")
 
 func item_description_set(inv_item: inventory_item):
 	$item_description/item_name.text = inv_item.item_name
